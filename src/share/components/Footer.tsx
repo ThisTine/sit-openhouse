8c0c1acb9d16299058f86ea36212e8fb50dedd47
Nine0512 <nineza0808@gmail.com>
2023-10-31 20:29:35 +0700

    :iphone: footer responsive

--- src/share/components/Footer.tsx
+++ src/share/components/Footer.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 import sit_logo from 'public/logo/sit_logo_bl.png';
 import Image from "next/image";
 import Location from '@/share/icon/Location';
-import Mail from '@/share/icon/Mail';
-import Facebook from '@/share/icon/Facebook';
-import Phone from '@/share/icon/Phone';
+import Mail from '@/share/icon/Mail.svg';
+import Facebook from '@/share/icon/Facebook.svg';
+import Phone from '@/share/icon/Phone.svg';
 import Line from '@/share/icon/line.svg';
 
 const Footer = () => {
 	return (
-		<div className="w-full bg-[#3E6493] py-16">
-			<div className="flex w-full justify-center pb-5">
+		<div className="w-full bg-[#3F6593] py-16">
+			<div className="flex w-full justify-center pb-5 max-lg:hidden">
 				<div className="grid w-[80%] grid-cols-3 gap-6 border-b-2 border-inherit pb-10">
 					<div>
 						<Image alt="sit logo" className="pb-5" src={sit_logo}/>
@@ -21,29 +21,29 @@ const Footer = () => {
 						</div>
 					</div>
 					<div className="col-span-2 text-gray-200">
-						<div className="grid grid-cols-3 gap-4 px-32">
+						<div className="grid grid-cols-3 px-0 xl:px-16 2xl:gap-8 2xl:px-32">
 							<div className="col-span-3">
 								<h1 className="text-2xl font-bold">Contact</h1>
 							</div>
 							<div>
 								<div className="mb-6 flex place-items-center gap-2 text-gray-200">
-									<Mail />
+									<Image alt='mail logo' src={Mail} />
 									<p>Info@sit.ac.th</p>
 								</div>
 								<h1 className="text-2xl font-bold">Open House</h1>
 								<div className="my-2 flex place-items-center gap-2 text-gray-200">
-									<Phone />
+									<Image alt='Phone logo' src={Phone} />
 									<p>063-4469879 (พี่สก็อต)</p>
 								</div>
 							</div>
 							<div>
 								<div className="mb-6 flex place-items-center gap-2 text-gray-200">
-									<Facebook/>
+									<Image alt='facebook logo' src={Facebook} />
 									<p>SIT.Family</p>
 								</div>
 								<h1 className="text-2xl font-bold">ICT</h1>
 								<div className="my-2 flex place-items-center gap-2 text-gray-200">
-									<Phone />
+									<Image alt='Phone logo' src={Phone} />
 									<p>02-470-9882 (พี่รุ่ง)</p>
 								</div>
 							</div>
@@ -54,7 +54,7 @@ const Footer = () => {
 								</div>
 								<h1 className="text-2xl font-bold">D-Day</h1>
 								<div className="my-2 flex place-items-center gap-2 text-gray-200">
-									<Phone />
+									<Image alt='Phone logo' src={Phone} />
 									<p>02-470-9832 (พี่นิคกี้)</p>
 								</div>
 							</div>
@@ -62,7 +62,38 @@ const Footer = () => {
 					</div>
 				</div>
 			</div>
-			<div className="flex justify-center text-gray-200 ">
+			<div className="flex w-[100%] justify-center lg:hidden">
+				<div className="mx-16 grid grid-cols-2 gap-2 border-b-2 border-inherit max-sm:mx-6">
+					<div className="col-span-2 text-center text-2xl font-bold text-gray-200">
+						<h1>Contect</h1>
+					</div>
+					<div>
+						<h1 className="text-base font-bold text-gray-200">Open House@SIT</h1>
+						<div className="my-2 flex place-items-center gap-2 text-gray-200">
+							<Image alt='Phone logo' src={Phone} />
+							<p>063-4469879 (พี่สก็อต)</p>
+						</div>
+					</div>
+					<div>
+						<h1 className="text-base font-bold text-gray-200">ICT@SIT</h1>
+						<div className="my-2 flex place-items-center gap-2 text-gray-200">
+							<Image alt='Phone logo' src={Phone} />
+							<p>02-470-9882 (พี่รุ่ง)</p>
+						</div>
+					</div>
+					<div>
+						<h1 className="text-base font-bold text-gray-200">D-Day@SIT</h1>
+						<div className="my-2 flex place-items-center gap-2 text-gray-200">
+							<Image alt='Phone logo' src={Phone} />
+							<p>02-470-9832 (พี่นิคกี้)</p>
+						</div>
+					</div>
+					<div className="font-bold text-gray-200">
+						<h1>126 ถนนประชาอุทิศ แขวงบางมด เขตทุ่งครุ กรุงเทพฯ 10140.</h1>
+					</div>
+				</div>
+			</div>
+			<div className="flex justify-center px-16 text-center text-gray-200 lg:px-0">
 				<h1 className="pt-2">© 2018 School of Information Technology, King Mongkut&apos;s University of Technology Thonburi.</h1>
 			</div>
 		</div>
--- src/share/icon/Phone.tsx
+++
@@ -1,8 +0,0 @@
-import React, { SVGProps } from 'react'
-
-export function MaterialSymbolsCall(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M19.95 21q-3.125 0-6.175-1.363t-5.55-3.862q-2.5-2.5-3.862-5.55T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.238t.325.562l.65 3.5q.05.4-.025.675T9.4 8.45L6.975 10.9q.5.925 1.187 1.787t1.513 1.663q.775.775 1.625 1.438T13.1 17l2.35-2.35q.225-.225.588-.338t.712-.062l3.45.7q.35.1.575.363T21 15.9v4.05q0 .45-.3.75t-.75.3Z"></path></svg>
-  )
-}
-export default MaterialSymbolsCall
