333d697540f123cac0bad9674da9128b48c3d428
Sunthewhat <81130744+Sunthewhat@users.noreply.github.com>
2023-11-01 14:06:59 +0700

    Agenda (#15)
    * [ADD] Agenda data
    
    * [ADD] implement Agenda page (inprogress)
    
    ---------
    
    Co-authored-by: Chotiwet Wisitworanat <chotiwet.wisi@icloud.com>

---
+++ src/app/agenda/data/ICT_Shorten.ts
@@ -0,0 +1,34 @@
+export const ICTChallengeShort = [
+	{
+		th: ["ลงทะเบียนเข้าร่วมงาน"],
+		en: ["Registration"],
+		start: 800,
+		end: 900
+	},
+	{
+		th: ["การแข่งขันรอบคัดเลือก"],
+		en: ["Qualifying Round"],
+		location: "LX 15 V-Space",
+		start: 900,
+		end: 1130
+	},
+	{
+		th: ["พักเที่ยง"],
+		en: ["Lunch Break"],
+		start: 1130,
+		end: 1245
+	},
+	{
+		th: ["ประกาศผลคะแนนรอบคัดเลือก"],
+		en: ["Announcement of the Qualifying Round"],
+		start: 1245,
+		end: 1300
+	},
+	{
+		th: ["แข่งขันรอบชิงชนะเลิศ", "ประกาศผล", "พิธีมอบรางวัล"],
+		en: ["Final Round", "Announcement of the Winner", "Award Ceremony"],
+		location: "LX 3 Auditorium",
+		start: 1300,
+		end: 1600
+	}
+];
