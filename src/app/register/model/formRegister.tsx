f304f89f375e94a4bcc0a981aa5855e017e5c239
Neng Sitiporn <jigneng1@hotmail.com>
2023-11-02 20:15:21 +0700

    add : modelForm

---
+++ src/app/register/model/formRegister.tsx
@@ -0,0 +1,11 @@
+interface ICTChallengeForm {
+    teamName : string,
+    schoolName : string,
+    schoolAddress : string,
+    ajarnPrefix : string,
+    ajarnName : string,
+    ajarnSurname : string,
+    phoneNum : string,
+    ajarnEmail : string,
+    ajarnPosition : string,
+}
