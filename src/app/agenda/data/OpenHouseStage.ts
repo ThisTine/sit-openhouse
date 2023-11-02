a8db2b1f9dd2b891e35195b9b9faf400cacf96fd
Chotiwet Wisitworanat <chotiwet.wisi@icloud.com>
2023-11-02 13:47:28 +0700

    [EDIT] Time table data

--- src/app/agenda/data/OpenHouseStage.ts
+++ src/app/agenda/data/OpenHouseStage.ts
@@ -2,85 +2,99 @@ import Agenda from "./agenda.model";
 const OpenHouseStage: Agenda[] = [
 	{
 		start: 800,
-		end: 830
+		end: 830,
+		height: 79
 	},
 	{
 		th: ["เริ่มงาน เล่าเกี่ยวกับ KMUTT"],
 		en: ["Opening Ceremony"],
 		start: 830,
-		end: 845
+		end: 845,
+		height: 34
 	},
 	{
 		th: ["สัมภาษณ์ซุ้มกิจกรรม"],
 		en: ["Interview with activity booths"],
 		start: 845,
-		end: 900
+		end: 900,
+		height: 34
 	},
 	{
 		th: ["พิธีกรให้ข้อมูลเกี่ยวกับ SIT"],
 		en: ["Information about SIT"],
 		start: 900,
-		end: 915
+		end: 915,
+		height: 34
 	},
 	{
 		th: ["กิจกรรมตอบคำถาม"],
 		en: ["Question and Answer"],
 		start: 915,
-		end: 945
+		end: 945,
+		height: 70
 	},
 	{
 		th: ["MUSIC"],
 		en: ["MUSIC"],
 		start: 945,
-		end: 1000
+		end: 1000,
+		height: 34
 	},
 	{
 		th: ["SIT MUSIC"],
 		en: ["SIT MUSIC"],
 		start: 1000,
-		end: 1015
+		end: 1015,
+		height: 34
 	},
 	{
 		th: ["แนะนำกิจกรรมสาขา"],
 		en: ["SIT Session talk"],
 		start: 1015,
-		end: 1100
+		end: 1100,
+		height: 105
 	},
 	{
 		th: ["SIT Alumni Talk"],
 		en: ["SIT Alumni Talk"],
 		start: 1100,
-		end: 1200
+		end: 1200,
+		height: 143
 	},
 	{
 		th: ["พักเที่ยง"],
 		en: ["Lunch Break"],
 		start: 1200,
-		end: 1245
+		end: 1245,
+		height: 105
 	},
 	{
 		th: ["Booths Tour"],
 		en: ["Booths Tour"],
 		start: 1245,
-		end: 1330
+		end: 1330,
+		height: 105
 	},
 	{
 		th: ["กิจกรรมตอบคำถาม"],
 		en: ["Question and Answer"],
 		start: 1330,
-		end: 1400
+		end: 1400,
+		height: 75
 	},
 	{
 		th: ["SIT MUSIC"],
 		en: ["SIT MUSIC"],
 		start: 1400,
-		end: 1445
+		end: 1445,
+		height: 100
 	},
 	{
 		th: ["กิจกรรมสันทนาการ"],
 		en: ["Entertainment Activity"],
 		start: 1445,
-		end: 1515
+		end: 1515,
+		height: 85
 	}
 ];
 
--- src/app/agenda/data/OpenHouseWorkshop.ts
+++ src/app/agenda/data/OpenHouseWorkshop.ts
@@ -2,60 +2,70 @@ import Agenda from "./agenda.model";
 const openHouseWorkshop: Agenda[] = [
 	{
 		start: 800,
-		end: 900
+		end: 900,
+		height: 155
 	},
 	{
 		th: ["บรรยาย Portfolio สาขา IT"],
 		en: ["IT. Major Portfolio Presentation"],
 		start: 900,
-		end: 920
+		end: 920,
+		height: 46
 	},
 	{
 		th: ["บรรยาย Portfolio สาขา CS"],
 		en: ["CS. Major Portfolio Presentation"],
 		start: 920,
-		end: 940
+		end: 940,
+		height: 46
 	},
 	{
 		th: ["บรรยาย Portfolio สาขา DSI"],
 		en: ["DSI. Major Portfolio Presentation"],
 		start: 940,
-		end: 1000
+		end: 1000,
+		height: 46
 	},
 	{
 		start: 1000,
-		end: 1030
+		end: 1030,
+		height: 70
 	},
 	{
 		th: ["Workshop สาขา DSI", "Get to know me 'DSI'"],
 		en: ["Workshop DSI", "Get to know me 'DSI'"],
 		location: "LX 12/1",
 		start: 1030,
-		end: 1130
+		end: 1130,
+		height: 145
 	},
 	{
 		th: ["พักเที่ยง"],
 		en: ["Lunch Break"],
 		start: 1130,
-		end: 1300
+		end: 1300,
+		height: 215
 	},
 	{
-		th: ["Workshop สาขา CS", "Easy & Fun Java (กิจกรรมภาษาอังกฤษ)"],
-		en: ["Workshop CS", "Easy & Fun Java (English Workshop)"],
+		th: ["Workshop สาขา CS", "Easy & Fun Java", "(กิจกรรมภาษาอังกฤษ)"],
+		en: ["Workshop CS", "Easy & Fun Java", "(English Workshop)"],
 		location: "LX 10/5",
 		start: 1300,
-		end: 1400
+		end: 1400,
+		height: 145
 	},
 	{
 		start: 1400,
-		end: 1415
+		end: 1415,
+		height: 34
 	},
 	{
 		th: ["Workshop สาขา IT", "ทำเว็บ Portfolio แบบบ้านๆ"],
 		en: ["Workshop IT", "Make your own Portfolio website"],
 		location: "LX 10/5",
 		start: 1415,
-		end: 1515
+		end: 1515,
+		height: 150
 	}
 ];
 
