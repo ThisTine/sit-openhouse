cc333937d87e245e5f564a5a2a4931853829634a
Chotiwet Wisitworanat <chotiwet.wisi@icloud.com>
2023-11-05 00:22:32 +0700

    [EDIT] Data

---
+++ src/app/agenda/data/DdayMobile.ts
@@ -0,0 +1,32 @@
+import Agenda from "./agenda.model";
+
+const DdayMobile: Agenda[] = [
+	{
+		start: 830,
+		end: 845,
+		th: ["ลงทะเบียน"],
+		en: ["Registration"]
+	},
+	{
+		start: 930,
+		end: 945,
+		th: ["พิธีเปิดงาน"],
+		en: ["Opening Ceremony"],
+		location: "LX 3rd floor"
+	},
+	{
+		start: 845,
+		end: 1600,
+		th: ["เดินชมงานตามอัธยาศัย"],
+		en: ["Attend the event at your leisure"]
+	},
+	{
+		start: 1545,
+		end: 1600,
+		th: ["พิธีปิดงาน"],
+		en: ["Closing Ceremony"],
+		location: "LX 3rd floor"
+	}
+];
+
+export default DdayMobile;
