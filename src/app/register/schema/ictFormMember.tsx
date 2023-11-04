9ca81e100aba67de5d68e9980e6226f82c219909
Neng Sitiporn <jigneng1@hotmail.com>
2023-11-04 10:36:34 +0700

    add : solve conflict

---
+++ src/app/register/schema/ictFormMember.tsx
@@ -0,0 +1,14 @@
+import { ObjectSchema, object, string } from "yup";
+import { ICTChallangeMemberForm } from "../model/formRegister";
+
+const ICTFormMemberSchema : ObjectSchema<ICTChallangeMemberForm>  = object().shape({
+	prefixMember : string().required("กรุณาใส่คำนำหน้า"),
+	memberName : string().required("กรุณาใส่ชื่อ"),
+	memberSurName: string().required("กรุณาใส่นามสกุล"),
+	gradeMember : string().required("กรุณาใส่ชั้นปี"),
+	phoneMember : string().required("กรุณาใส่เบอร์โทรศัพท์"),
+	emailMember : string().required("กรุณาใส่อีเมล"),
+	facebookMember : string().required("กรุณาใส่เฟสบุ๊ค"),
+	lineMember : string().required("กรุณาใส่ไลน์")
+    
+});
