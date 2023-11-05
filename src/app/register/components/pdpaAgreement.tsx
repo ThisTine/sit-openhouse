213c8e7f865515fa050ba33ef22f37dedbf91866
Neng Sitiporn <jigneng1@hotmail.com>
2023-11-05 10:54:10 +0700

    Ict branch (#36)
    * fix : ui pdpa
    
    * add : model
    
    * add : pdpa routes
    
    * API ยิงจริงไม่ติงนัง
    
    * add : form API
    
    * add : fail register
    
    * edit file name
    
    * add : failRegisterPage

--- src/app/register/components/pdpaAgreement.tsx
+++ src/app/register/components/pdpaAgreement.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@mui/material";
+import React from "react";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import Image from "next/image";
-import React from "react";
-import PdpaText from "../components/pdpaText";
-
-const PDPAagreementPage = () => {
+import PdpaText from "./pdpaText";
+import { Dispatch } from "react";
+import { SetStateAction } from "react";
+import { registerPage } from "../model/formRegister";
+interface PDPAagreementPageProps {
+    setPage : Dispatch<SetStateAction<registerPage>>;
+}
+const PDPAagreementPage = ({setPage} : PDPAagreementPageProps) => {
 	return (
 		<div style={{backgroundColor:"#34312F"}}>
 			<div className="p-5">
@@ -18,9 +23,9 @@ const PDPAagreementPage = () => {
 					<Image alt="sit-logo" height={200} src="/assets/sit_logo.png" width={200} />
 				</div>
 				<div>
-					<h2 className="my-5 text-xl text-primary">หนังสือยินยอมเพื่อขอใช้ประโยชน์
+					<h2 className="my-5 text-xl text-primary  md:text-5xl">หนังสือยินยอมเพื่อขอใช้ประโยชน์
                     ข้อมูลส่วนบุคคล</h2>
-					<div className="my-5 text-gray-3">
+					<div className="my-8 text-gray-3 md:flex">
 						<p>คณะเทคโนโลยีสารสนเทศ</p>
 						<p>มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี</p>
 					</div>
@@ -30,9 +35,9 @@ const PDPAagreementPage = () => {
 						<PdpaText/>
 					</div>
 				</div>
-				<div className="my-5 flex flex-col gap-5">
-					<Button className="h-12" fullWidth variant="outlined">ย้อนกลับ</Button>
-					<Button className="h-12 bg-primary" variant="contained">ยอมรับและยื่นยันการลงทะเบียน</Button>
+				<div className="my-5 flex flex-col gap-5  md:flex-row md:justify-between">
+					<Button className="h-12"  variant="outlined">ย้อนกลับ</Button>
+					<Button className="h-12 bg-primary" onClick={()=>setPage(registerPage.ictFormPage)} variant="contained">ยอมรับและยื่นยันการลงทะเบียน</Button>
 				</div>
 			</div>
 		</div>);
--- src/app/register/solution-chllenge/page.tsx
+++ src/app/register/solution-chllenge/page.tsx
@@ -7,61 +7,80 @@ import { grade, prefix } from "../constant/formConst";
 import { useForm } from 'react-hook-form';
 import { yupResolver } from "@hookform/resolvers/yup";
 import { ictFormAddSchema } from "../schema/ictFormSchema";
-import { ICTChallengeForm } from "../model/formRegister";
+import { ICTChallengeForm, registerPage } from "../model/formRegister";
 import { useState } from "react";
 import React from "react";
 import TeamInfoForm from "../components/teamInfoForm";
+import CongratICT from "../components/CongratICT";
+import PDPAagreementPage from "../components/pdpaAgreement";
+import FailRegister from "../components/FailRegister";
+
 const SolutionFormPage = () => {
 	const { handleSubmit, control,formState: { errors } } = useForm({resolver : yupResolver(ictFormAddSchema)});
-	// const [isSuccess, setIsSuccess] = useState(false);
-	//  function submited(){
-	// 	const onSubmit = (data : ICTChallengeForm) => {
-	// 		setIsSuccess(true);
-	// 		console.log(data);
-	// 	};
-	return (
-		// <CongratICT/>
-		//   ! isSuccess ? (
-		  <div style={{backgroundColor:"#34312F" , padding:'30px'}}>
-			<div className="p-5">
-				<div className="flex justify-between py-3">
-					<div>
-						<Button>
-							<ArrowBackIosNewIcon className="text-white" />
-							<p className="text-xl text-text-primary">Back</p>
-						</Button>
-					</div>
+	const [Page , setPage] = useState<registerPage>(registerPage.pdapaPage);
+
+	// <CongratICT/>
+	//   ! isSuccess ? (
+	if(Page === registerPage.ictFormPage) {
+		return(
+			<div style={{backgroundColor:"#34312F" , padding:'30px'}}>
+				<div className="p-5">
+					<div className="flex justify-between py-3">
+						<div>
+							<Button onClick={()=>setPage(registerPage.pdapaPage)}>
+								<ArrowBackIosNewIcon className="text-white" />
+								<p className="text-xl text-text-primary">Back</p>
+							</Button>
+						</div>
 				
-				</div>
-				<div className="w-72 md:w-full">
-					<h1 className="text-3xl font-bold" style={{color:"#3399CC"}}>แบบฟอร์มลงทะเบียน ICT Challenge 2023 </h1>
-				</div>
-				<div>
-					<p className="py-3  text-gray-3">
+					</div>
+					<div className="w-72 md:w-full">
+						<h1 className="text-3xl font-bold" style={{color:"#3399CC"}}>แบบฟอร์มลงทะเบียน ICT Challenge 2023 </h1>
+					</div>
+					<div>
+						<p className="py-3  text-gray-3">
 	  			ก่อนนี้เธอคงจะปวดร้าว ต้องฝืนอดทนว่าไม่รู้ตัว คงไม่มีทางหรอก 
 	  			แต่เธอแค่ไม่กล้าบอก ก่อนนี้โลกดูช่างสดใส ได้ฝืนอดทนไม่ให้รู้ตัว 
 	  			เธอนั้นสำคัญเท่าไรฉันแค่ไม่กล้าบอกให้รู้ ฮู้ คงต้องเข้าใจหากฉันไม่บอกไป
 	  			เธอคงฝืนทนอยู่อย่างนี้เหมือนเดิมอยู่อย่างนี้ คงไม่เปลี่ยนไปหากฉันยื้อต่อไป
 	  			ให้เธอหายไปจากตรงนี้ให้เธอหายไปไม่ย้อนคืน
-					</p>
-				</div>
-				<div className="relative my-3 rounded-xl bg-primary">
-					<div className="relative bottom-3 right-3 rounded-xl bg-gray-2">
-						<TeamInfoForm formControl={control} formErrors={errors} grade={grade} prefix={prefix}/>
+						</p>
+					</div>
+					<div className="relative my-3 rounded-xl bg-primary">
+						<div className="relative bottom-3 right-3 rounded-xl bg-gray-2">
+							<TeamInfoForm formControl={control} formErrors={errors} grade={grade} prefix={prefix}/>
+						</div>
+					</div>
+					<div className="m-2">
+						<p className="flex justify-end text-text-primary">** กรุณาตรวจสอบความถูกต้องให้ครบถ้วนก่อนกดยืนยัน
+	      หากกดยืนยันแล้วจะไม่สามารถแก้ไขข้อมูลการสมัครได้</p>
+					</div>
+					<div>
+						<ConfirmModal handleOnSubmit = {handleSubmit} setPage = {setPage} />
 					</div>
 				</div>
-				<div className="m-2">
-					<p className="flex justify-end text-text-primary">** กรุณาตรวจสอบความถูกต้องให้ครบถ้วนก่อนกดยืนยัน
-	      หากกดยืนยันแล้วจะไม่สามารถแก้ไขข้อมูลการสมัครได้</p>
-				</div>
-				<div>
-					<ConfirmModal handleOnSubmit = {handleSubmit}/>
-				</div>
-			</div>
-		</div> 
+			</div> 
+		);
+	}
+	else if (Page === registerPage.pdapaPage){
+		return(
+			<PDPAagreementPage setPage={setPage}/>
+		);
+	}
+
+	else if (Page === registerPage.congratsIct){
+		return(
+			<CongratICT/>
+		);
+	}
+	else if (Page === registerPage.failCongrats){
+		return(
+			<FailRegister/>
+		);
+	}
 			
 	// ) : ( <CongratICT/> )
-	);
+	
 };
 // };
 
