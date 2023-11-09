5e73e0f3b3cdf688e10cc914cd5d0f19ba115380
Phurichaya Khemvaraporn <phurichayakhem1@gmail.com>
2023-11-09 20:08:34 +0700

    update : prefix

--- src/app/register/constant/formConst.ts
+++ src/app/register/constant/formConst.ts
@@ -1,3 +1,3 @@
-export const prefix = [{value: "นาย",label: "นาย"},{value: "นางสาว",label: "นางสาว"},{value: "เด็กชาย",label: "เด็กชาย"},{value: "เด็กหญิง",label: "เด็กหญิง"}];
+export const prefix = [{value: "นาย",label: "นาย"},{value: "นางสาว",label: "นางสาว"},{value: "เด็กชาย",label: "เด็กชาย"},{value: "เด็กหญิง",label: "เด็กหญิง"}, {value: "นาง",label: "นาง"}];
 
 export const grade = [{value: "มัธยมศึกษาปีที่ 4",label: "มัธยมศึกษาปีที่ 4"},{value: "มัธยมศึกษาปีที่ 5",label: "มัธยมศึกษาปีที่ 5"},{value: "มัธยมศึกษาปีที่ 6",label: "มัธยมศึกษาปีที่ 6"}];
