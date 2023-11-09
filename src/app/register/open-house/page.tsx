9c35e3029a4624343fac45c69331e3982bc2caeb
sittichok Ouamsiri <sutichok46@gmail.com>
2023-11-10 01:51:25 +0700

    [FIX] add default value

--- src/app/register/open-house/page.tsx
+++ src/app/register/open-house/page.tsx
@@ -2,14 +2,18 @@ import 'server-only';
 import { ISheetData} from "@/app/api/utils/getSheetNum";
 import Form from "./Form";
 async function getData() {
-	const res = await fetch("http://localhost:3000/api/register/open-house/limit");
+	try{
+		const res = await fetch("http://localhost:3000/api/register/open-house/limit", {cache:"no-store"});
 	
-	if(!res.ok){
-		throw new Error("There's a problem in our side, please try again");
-	}	
-	const data = await res.json();
-	console.log(data);
-	return data as Promise<ISheetData>;
+		if(!res.ok){
+			throw new Error("There's a problem in our side, please try again");
+		}
+		const data = await res.json();	
+		return data as ISheetData;
+	}catch(err){
+		return {dsi:0,java:0,web:0} as ISheetData;
+	}
+		
 } 
 
 export default async function Page(){
